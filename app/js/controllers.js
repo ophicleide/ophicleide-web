4834526d812099ef7208f0a3459651045228ce97
Michael McCune <msm@redhat.com>
2016-09-09 22:23:04 -0400

    refactor alerts
    This change moves the alert functionality into a factory object to make
    the code cleaner and more idiomatic angular. This will isolate the logic
    for updating the alerts into a reusable service.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,13 +6,7 @@ var module = angular.module("Ophicleide.controllers", [
 ]);
 
 module.controller("BodyCtrl", ["$scope", "$rootScope", function($scope, $rootScope) {
-  $scope.alerts = [];
-  $scope.removeAlert = function(index) {
-    $scope.alerts.splice(index, 1);
-  };
-  $rootScope.addAlert = function(title, msg) {
-    $scope.alerts.push({title: title, msg: msg});
-  };
+  $rootScope.alerts = [];
 }]);
 
 module.controller("NavCtrl", ["$scope", "$location", function($scope, $location) {
@@ -25,7 +19,7 @@ module.controller("ModelsCtrl", ["$scope", "modelActions", function($scope, mode
   angular.extend($scope, modelActions);
 }]);
 
-module.controller("ModelsModalCtrl", ["$scope", "$rootScope", "$uibModalInstance", "$log", function($scope, $rootScope, $uibModalInstance, $log) {
+module.controller("ModelsModalCtrl", ["$scope", "$rootScope", "$uibModalInstance", "$log", "alertActions", function($scope, $rootScope, $uibModalInstance, $log, alertActions) {
   var fields = {
     name: "",
     modelUrls: "",
--- app/js/factories.js
+++ app/js/factories.js
@@ -29,3 +29,39 @@ module.factory("queryActions", ["$uibModal", function($uibModal) {
     newQuery: newQuery,
   };
 }]);
+
+module.factory("alertActions", ["$rootScope", function($rootScope) {
+  removeAlert = function(index) {
+    $rootScope.alerts.splice(index, 1);
+  };
+  addSuccessAlert = function(title, msg) {
+    $rootScope.alerts.push({
+      title: title,
+      msg: msg,
+      severity: "alert-success",
+      icon: "pficon-ok",
+    });
+  };
+  addDangerAlert = function(title, msg) {
+    $rootScope.alerts.push({
+      title: title,
+      msg: msg,
+      severity: "alert-danger",
+      icon: "pficon-error-circle-o",
+    });
+  };
+  addWarningAlert = function(title, msg) {
+    $rootScope.alerts.push({
+      title: title,
+      msg: msg,
+      severity: "alert-warning",
+      icon: "pficon-warning-triangle-o",
+    });
+  };
+  return {
+    removeAlert: removeAlert,
+    addSuccessAlert: addSuccessAlert,
+    addDangerAlert: addDangerAlert,
+    addWarningAlert: addWarningAlert,
+  };
+}]);
