8aaf0ae7ed409ca393bc8c02917291167293ea4d
Michael McCune <msm@redhat.com>
2016-09-13 09:32:10 -0400

    add some basic validation to new model form
    This change adds some simple blank field detection to the modal form for
    new training models.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -22,14 +22,48 @@ module.controller("ModelsCtrl", ["$scope", "modelActions", function($scope, mode
 module.controller("ModelsModalCtrl", ["$scope", "$rootScope", "$uibModalInstance", "$log", "alertActions", function($scope, $rootScope, $uibModalInstance, $log, alertActions) {
   var fields = {
     name: "",
-    modelUrls: "",
+    urls: "",
+    nameEmpty: false,
+    urlsEmpty: false,
   };
   $scope.fields = fields;
   $scope.ok = function() {
-    $uibModalInstance.close();
+    if ($scope.fields.name === "") {
+      $scope.fields.nameEmpty = true;
+    }
+    else {
+      $scope.fields.nameEmpty = false;
+    }
+    if ($scope.fields.urls === "") {
+      $scope.fields.urlsEmpty = true;
+    }
+    else {
+      $scope.fields.urlsEmpty = false;
+    }
+    if ($scope.fields.nameEmpty === false &&
+        $scope.fields.urlsEmpty === false) {
+      $uibModalInstance.close();
+    }
   };
   $scope.cancel = function() {
     $uibModalInstance.dismiss();
+  };
+  $scope.classHasError = function(group) {
+    if ($scope.hasError(group)) {
+      return "has-error";
+    }
+  };
+  $scope.hasError = function(group) {
+    var ret = false;
+    switch (group) {
+      case "name":
+        ret = $scope.fields.nameEmpty;
+        break;
+      case "urls":
+        ret = $scope.fields.urlsEmpty;
+        break;
+    }
+    return ret;
   };
 }]);
 
