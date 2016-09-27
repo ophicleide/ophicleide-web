f1d9d9a50b536d37548b49f5e19be5f041b83618
Michael McCune <msm@redhat.com>
2016-09-27 14:35:32 -0400

    add removeAlerts to the body controller
    This is to ensure that the alert close button will properly remove the
    alerts from the internal list.

--- app/js/controllers/body.js
+++ app/js/controllers/body.js
@@ -1,6 +1,14 @@
 "use strict";
 
 angular.module("ophicleideWeb")
-  .controller("BodyCtrl", ["$scope", "$rootScope", function($scope, $rootScope) {
+  .controller("BodyCtrl", [
+      "$scope",
+      "$rootScope",
+      "alertActions",
+      function(
+        $scope,
+        $rootScope,
+        alertActions) {
     $rootScope.alerts = [];
+    $scope.removeAlert = alertActions.removeAlert;
   }]);
