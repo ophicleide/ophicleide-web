4098241172c6486c22409f04c00d8904776a62c1
Daniel Soukhov <dsoukhov@redhat.com>
2017-06-22 11:50:02 -0400

    Added basic unit tests
    Tests are done using the mocha testframe work and karma is used
    as the test launcher. Factories and controllers were tested.

---
+++ test/factoriesTest.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var modelActions;
+var queryActions;
+var $http;
+var modelsPath = "/api/models";
+var queriesPath = "/api/queries";
+
+describe('Testing rest apis of factories', function() {
+
+    beforeEach(module('ophicleideWeb'));
+
+    beforeEach(inject(function(_modelActions_, _queryActions_, _$http_) {
+        modelActions = _modelActions_;
+        queryActions = _queryActions_;
+        $http = _$http_;
+        sinon.spy($http, 'post');
+        sinon.spy($http, 'get');
+        sinon.spy($http, 'delete');
+    }));
+
+    describe('Testing rest modelActions api paths', function() {
+
+        it('modelActions delete rest test', function() {
+            modelActions.deleteModel(1);
+            chai.expect($http.delete.getCall(0).args[0]).to.equal(modelsPath + '/1');
+            chai.expect($http.delete.calledOnce).to.true;
+        });
+
+        it('modelAction getModels rest test', function() {
+            modelActions.getModels();
+            chai.expect($http.get.getCall(0).args[0]).to.equal(modelsPath);
+            chai.expect($http.get.calledOnce).to.true;
+        });
+
+        it('modelAction createModels rest test', function() {
+            modelActions.createModel();
+            chai.expect($http.post.getCall(0).args[0]).to.equal(modelsPath);
+            chai.expect($http.post.calledOnce).to.true;
+        });
+    });
+
+    describe('Testing rest queryActions api paths', function() {
+
+        it('queryAction createQuery rest api test', function() {
+            queryActions.createQuery();
+            chai.expect($http.post.getCall(0).args[0]).to.equal(queriesPath);
+            chai.expect($http.post.calledOnce).to.true;
+        });
+
+        it('queryAction getQuery rest api test', function() {
+            queryActions.getQueries();
+            chai.expect($http.get.getCall(0).args[0]).to.equal(queriesPath);
+            chai.expect($http.get.calledOnce).to.true;
+        });
+    });
+});
